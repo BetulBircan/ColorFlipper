0406db7a7501f076dfc37902608e77afb226cf02
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-12 15:13:27 +0300

    rastgele renk üretmesi için fonksiyon oluşturuldu

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const text = document.querySelector('#text')
 //butındaki class özelliğini atama
 const btn = document.querySelector('.btn')
 
-//rastgele hexadecimal kod üretmesi iiçin oluşturulan dizi
+//rastgele hexadecimal kod üretmesi için oluşturulan dizi
 const hexaCode = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+//rastgele renk üretmesi için oluşturulan fonksiyon
+const randomColor = () => {
+    let hexCode = "#"
+
+    for(let i = 0; i < 6; i++)  {
+        hexCode += hexaCode[Math.floor(Math.random() * hexaCode.length)]
+    }
+    return hexCode
+}
