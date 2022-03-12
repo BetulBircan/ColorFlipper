d0a1312c3bacd39864b2683e5fd48ac70b93f87a
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-12 15:10:13 +0300

    app.js dosyası index.html e eklendi ve button span ve rastgele kod üretmesi için hexaCode adlı dizi tanımlandı

---
+++ app.js
@@ -0,0 +1,7 @@
+//spandaki id özelliğini atama
+const text = document.querySelector('#text')
+//butındaki class özelliğini atama
+const btn = document.querySelector('.btn')
+
+//rastgele hexadecimal kod üretmesi iiçin oluşturulan dizi
+const hexaCode = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
